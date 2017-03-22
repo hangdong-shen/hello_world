09634999b3eb5c235ac03763e4c10c4713e66936
hangdong-shen <eastward_nj@foxmail.com>
2017-03-22 15:00:43 +0800

    Create hello_world.java

---
+++ hello_world.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.test;
+
+/**
+ * @author root
+ *
+ */
+ 
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("hello world!");
+	}
